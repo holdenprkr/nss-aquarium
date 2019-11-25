15f202ca6b324ad3e22abe1d8bc1cbd844e2d7f7
holdenprkr <holdenprkr@gmail.com>
2019-11-25 11:58:36 -0600

    making website mobile responsive

--- scripts/fish/dialogs.js
+++ scripts/fish/dialogs.js
@@ -17,55 +17,19 @@ const initializeDetailButtonEvents = () => {
   // You will be writing code below this line
 
   // Show Bart's details when the button is clicked
-  document.querySelector("#button--Bitey").addEventListener(
-    "click",
-    theClickEvent => {
-      const theDialog = document.querySelector("#details--Bitey")
-      theDialog.showModal()
-    }
-  )
+  const allDetailButtons = document.querySelectorAll("button[id^='button--']")
 
-  document.querySelector("#button--Jawsie").addEventListener(
-    "click",
-    theClickEvent => {
-      const theDialog = document.querySelector("#details--Jawsie")
-      theDialog.showModal()
-    }
-  )
-
-  document.querySelector("#button--Angie").addEventListener(
-    "click",
-    theClickEvent => {
-      const theDialog = document.querySelector("#details--Angie")
-      theDialog.showModal()
-    }
-  )
-
-  document.querySelector("#button--Barry").addEventListener(
-    "click",
-    theClickEvent => {
-      const theDialog = document.querySelector("#details--Barry")
-      theDialog.showModal()
-    }
-  )
-
-  document.querySelector("#button--Spongebob").addEventListener(
-    "click",
-    theClickEvent => {
-      const theDialog = document.querySelector("#details--Spongebob")
-      theDialog.showModal()
-    }
-  )
-
-  document.querySelector("#button--Sandy").addEventListener(
-    "click",
-    theClickEvent => {
-      const theDialog = document.querySelector("#details--Sandy")
-      theDialog.showModal()
-    }
-  )
-
-
-};
+  // Add an event listener to each one
+  for (const btn of allDetailButtons) {
+      btn.addEventListener(
+          "click",
+          theEvent => {
+              const dialogSiblingSelector = `#${theEvent.target.id}+dialog`
+              const theDialog = document.querySelector(dialogSiblingSelector)
+              theDialog.showModal()
+          }
+      )
+  }
+}
 
   export default initializeDetailButtonEvents
